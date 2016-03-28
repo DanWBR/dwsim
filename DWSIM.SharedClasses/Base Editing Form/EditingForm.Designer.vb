2260d830b48daade7cafe2462fe07e7378f6152b
DanWBR <danielwag@gmail.com>
2016-03-28 15:51:37 -0400

    Added Editing Form skeleton

---
+++ DWSIM.SharedClasses/Base Editing Form/EditingForm.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EditingForm
+    Inherits WeifenLuo.WinFormsUI.Docking.DockContent
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Contents = New System.Windows.Forms.FlowLayoutPanel()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(395, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(30, 30)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "< >"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Contents
+        '
+        Me.Contents.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Contents.Location = New System.Drawing.Point(4, 5)
+        Me.Contents.Name = "Contents"
+        Me.Contents.Size = New System.Drawing.Size(385, 392)
+        Me.Contents.TabIndex = 1
+        '
+        'EditingForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(431, 400)
+        Me.Controls.Add(Me.Contents)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "EditingForm"
+        Me.ShowHint = WeifenLuo.WinFormsUI.Docking.DockState.DockLeft
+        Me.ShowIcon = False
+        Me.Text = "EditingForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Contents As System.Windows.Forms.FlowLayoutPanel
+End Class
---
+++ DWSIM.SharedClasses/Base Editing Form/EditingForm.vb
@@ -0,0 +1,8 @@
+﻿Public Class EditingForm
+
+    Inherits WeifenLuo.WinFormsUI.Docking.DockContent
+
+    Private Sub EditingForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
