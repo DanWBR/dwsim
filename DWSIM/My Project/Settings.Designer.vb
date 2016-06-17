bb6cfac462a411e76a38a47b8a8f68a34f90b5d2
DanWBR <danielwag@gmail.com>
2016-06-17 15:24:35 -0400

    Update

--- DWSIM/My Project/Settings.Designer.vb
+++ DWSIM/My Project/Settings.Designer.vb
@@ -616,7 +616,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
         Public Property CloseFormsOnDeselecting() As Boolean
             Get
                 Return CType(Me("CloseFormsOnDeselecting"),Boolean)
