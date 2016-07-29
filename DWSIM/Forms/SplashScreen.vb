65c4770a7e9227197426a034cf4aae0388dbd545
DanWBR <danielwag@gmail.com>
2016-07-29 15:26:01 -0400

    Update

--- DWSIM/Forms/SplashScreen.vb
+++ DWSIM/Forms/SplashScreen.vb
@@ -25,19 +25,6 @@ Public NotInheritable Class SplashScreen
 
     Private Sub SplashScreen_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
-
-        'Set up the dialog text at runtime according to the application's assembly information.  
-
-        'TODO: Customize the application's assembly information in the "Application" pane of the project 
-        '  properties dialog (under the "Project" menu).
-
-        'Format the version information using the text set into the Version control at design time as the
-        '  formatting string.  This allows for effective localization if desired.
-        '  Build and revision information could be included by using the following code and changing the 
-        '  Version control's designtime text to "Version {0}.{1:00}.{2}.{3}" or something similar.  See
-        '  String.Format() in Help for more information.
-        '
-        '    Version.Text = System.String.Format(Version.Text, My.Application.Info.Version.Major, My.Application.Info.Version.Minor, My.Application.Info.Version.Build, My.Application.Info.Version.Revision)
         Dim updfile = My.Application.Info.DirectoryPath & Path.DirectorySeparatorChar & "version.info"
         Version.Text = "Version " & My.Application.Info.Version.Major & "." & My.Application.Info.Version.Minor
         If File.Exists(updfile) Then
@@ -47,8 +34,6 @@ Public NotInheritable Class SplashScreen
         End If
         If My.Settings.PreviewVersion <> "" Then Version.Text += " " & My.Settings.PreviewVersion
 
-        'Copyright info
-        'Copyright.Text = My.Application.Info.Copyright
     End Sub
 
     Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
