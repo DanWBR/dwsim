3c23f21ea383ccb168ec24361ee1169e3d54dc5e
DanWBR <danielwag@gmail.com>
2016-08-04 14:30:48 -0400

    Auto-Update enabled by default

--- DWSIM/My Project/Settings.Designer.vb
+++ DWSIM/My Project/Settings.Designer.vb
@@ -628,7 +628,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
         Public Property AutomaticUpdates() As Boolean
             Get
                 Return CType(Me("AutomaticUpdates"),Boolean)
