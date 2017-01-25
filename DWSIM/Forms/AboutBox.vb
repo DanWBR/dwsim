c46a208bb065c247991363f4c934366eab71f51b
DanWBR <danielwag@gmail.com>
2017-01-25 10:58:54 -0400

    including automation dll in about box

--- DWSIM/Forms/AboutBox.vb
+++ DWSIM/Forms/AboutBox.vb
@@ -149,7 +149,7 @@ Public Class AboutBox
 
         'get DWSIM components' versions
 
-        Dim assnames = New String() {"DWSIM.exe", "DWSIM.DrawingTools.dll", "DWSIM.ExtensionMethods.dll", "DWSIM.FileDownloader.dll",
+        Dim assnames = New String() {"DWSIM.exe", "DWSIM.Automation.dll", "DWSIM.DrawingTools.dll", "DWSIM.ExtensionMethods.dll", "DWSIM.FileDownloader.dll",
                                      "DWSIM.FlowsheetSolver.dll", "DWSIM.GlobalSettings.dll", "DWSIM.Interfaces.dll", "DWSIM.MathOps.dll",
                                      "DWSIM.SharedClasses.dll", "DWSIM.Thermodynamics.dll", "DWSIM.Thermodynamics.NativeLibraries.dll",
                                      "DWSIM.UnitOperations.dll", "DWSIM.Updater.exe", "DWSIM.XMLSerializer.dll"}
