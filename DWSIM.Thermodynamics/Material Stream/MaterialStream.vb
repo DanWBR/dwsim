5ca0151b54a84c93bfe9f342f6c40269c3c18e76
DanWBR <danielwag@gmail.com>
2017-08-17 15:12:26 -0400

    [NEwUI] Fixed heat capacity/enthalpy/entropy units in Material Stream reports

--- DWSIM.Thermodynamics/Material Stream/MaterialStream.vb
+++ DWSIM.Thermodynamics/Material Stream/MaterialStream.vb
@@ -6204,7 +6204,12 @@ Namespace Streams
                 If d.Key.Contains("SPACE") Then
                     results.TextOutput += System.Environment.NewLine
                 Else
-                    results.TextOutput += (id.PadRight(40) & d.Value.ToArray.ToArrayString(CultureInfo.CurrentUICulture, numberformat).PadRight(20) & vbTab & results.DataUnits(d.Key)) & System.Environment.NewLine
+                    If d.Key.ToLower.Contains("heat capacity") Or d.Key.ToLower.Contains("enthalpy") Or d.Key.ToLower.Contains("entropy") Then
+                        d.Value(0) /= 1000
+                        results.TextOutput += (id.PadRight(40) & d.Value.ToArray.ToArrayString(CultureInfo.CurrentUICulture, numberformat).PadRight(20) & vbTab & results.DataUnits(d.Key)) & System.Environment.NewLine
+                    Else
+                        results.TextOutput += (id.PadRight(40) & d.Value.ToArray.ToArrayString(CultureInfo.CurrentUICulture, numberformat).PadRight(20) & vbTab & results.DataUnits(d.Key)) & System.Environment.NewLine
+                    End If
                 End If
             Next
 
