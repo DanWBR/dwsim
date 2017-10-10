d231d345a6787a7088720aa1ded6d6966b5725f1
DanWBR <danielwag@gmail.com>
2017-10-10 08:59:36 -0400

    updated version numbers

--- DWSIM.ExtensionMethods.Eto/Properties/AssemblyInfo.cs
+++ DWSIM.ExtensionMethods.Eto/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DWSIM.ExtensionMethods.Eto")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DWSIM Extension Methods (Eto.Forms)")]
+[assembly: AssemblyDescription("Extension Methods for DWSIM Simulator (Eto.Forms)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DWSIM.ExtensionMethods.Eto")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Daniel Medeiros")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyVersion("5.1.*")]
--- DWSIM/My Project/AssemblyInfo.vb
+++ DWSIM/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("5.0.*")> 
+<Assembly: AssemblyVersion("5.1.*")> 
 '<Assembly: AssemblyFileVersion("1.6")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("pt-BR")> 
