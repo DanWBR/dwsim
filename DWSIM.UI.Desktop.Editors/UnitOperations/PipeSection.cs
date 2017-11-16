b210e0434ca995c1c051a2438ee68566d2dce8b2
DanWBR <danielwag@gmail.com>
2017-11-16 09:57:39 -0400

    [NewUI] Pipe Segment: fixed section diameter conversion

--- DWSIM.UI.Desktop.Editors/UnitOperations/PipeSection.cs
+++ DWSIM.UI.Desktop.Editors/UnitOperations/PipeSection.cs
@@ -85,8 +85,8 @@ namespace DWSIM.UI.Desktop.Editors
             var txtIncrements = container.CreateAndAddTextBoxRow("N0", "Increments", section.Incrementos, (sender, e) => { if (double.TryParse(sender.Text.ToString(), out dummy)) section.Incrementos = int.Parse(sender.Text.ToString());});
             var txtLength = container.CreateAndAddTextBoxRow(nf, "Length" + " (" + su.distance + ")", cv.ConvertFromSI(su.distance, section.Comprimento), (sender, e) => { if (double.TryParse(sender.Text.ToString(), out dummy)) section.Comprimento = cv.ConvertToSI(su.distance, double.Parse(sender.Text.ToString())); });
             var txtElevation = container.CreateAndAddTextBoxRow(nf, "Elevation" + " (" + su.distance + ")", cv.ConvertFromSI(su.distance, section.Elevacao), (sender, e) => { if (double.TryParse(sender.Text.ToString(), out dummy)) section.Elevacao = cv.ConvertToSI(su.distance, double.Parse(sender.Text.ToString())); });
-            var txtED = container.CreateAndAddTextBoxRow(nf, "External Diameter" + " (" + su.diameter + ")", cv.ConvertFromSI(su.diameter, section.DE), (sender, e) => { if (double.TryParse(sender.Text.ToString(), out dummy)) section.DE = cv.ConvertToSI(su.diameter, double.Parse(sender.Text.ToString())); });
-            var txtID = container.CreateAndAddTextBoxRow(nf, "Internal Diameter" + " (" + su.diameter + ")", cv.ConvertFromSI(su.diameter, section.DI), (sender, e) => { if (double.TryParse(sender.Text.ToString(), out dummy)) section.DI = cv.ConvertToSI(su.diameter, double.Parse(sender.Text.ToString())); });
+            var txtED = container.CreateAndAddTextBoxRow(nf, "External Diameter" + " (" + su.diameter + ")", cv.Convert("in", su.diameter, section.DE), (sender, e) => { if (double.TryParse(sender.Text.ToString(), out dummy)) section.DE = cv.Convert(su.diameter, "in", double.Parse(sender.Text.ToString())); });
+            var txtID = container.CreateAndAddTextBoxRow(nf, "Internal Diameter" + " (" + su.diameter + ")", cv.Convert("in", su.diameter, section.DI), (sender, e) => { if (double.TryParse(sender.Text.ToString(), out dummy)) section.DI = cv.Convert(su.diameter, "in", double.Parse(sender.Text.ToString())); });
      
 
 
