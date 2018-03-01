eae1fab5f45b1915c75b046c487c239963d7c569
DanWBR <danielwag@gmail.com>
2018-03-01 07:56:29 -0400

    Created DWSIM.Inspector project

---
+++ DWSIM.Inspector/Inspector.vb
@@ -0,0 +1,27 @@
+Public Class Collection
+
+    Public Shared Results As New Dictionary(Of String, List(Of InspectorItem))
+
+    Public Shared CurrentItemID As String = ""
+
+End Class
+
+Public Class InspectorItem
+
+    Public Property Name As String = ""
+    Public Property Description As String = ""
+
+    Public Property HTMLParagraphs As New List(Of String)
+
+    Public Property CalculationRequestID As String = ""
+    Public Property ThreadID As Integer = -1
+
+    Public Property Time As DateTime = DateTime.Now
+
+    Public Property Items As New List(Of InspectorItem)
+
+    Sub New()
+
+    End Sub
+
+End Class
