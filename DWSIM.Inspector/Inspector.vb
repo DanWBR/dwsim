9a85a23e3d58faeb31c9df59fca6a4dbf70447d7
Daniel <danielwag@gmail.com>
2018-04-05 22:36:46 -0400

    [WIP] inspector

--- DWSIM.Inspector/Inspector.vb
+++ DWSIM.Inspector/Inspector.vb
@@ -108,7 +108,7 @@ Public Class InspectorItem
         If GlobalSettings.Settings.RunningPlatform = GlobalSettings.Settings.Platform.Windows Then
             stb.AppendLine("<script type='text/javascript' async src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/MathJax.js?config=default'></script>")
         Else
-            stb.AppendLine("<script type='text/x-mathjax-config'> MathJax.Hub.Config({ 'CommonHTML': { scale: 100, linebreaks: { automatic: true } }, SVG: { linebreaks: { automatic:true } }, displayAlign: 'left' }); </script>")
+            stb.AppendLine("<script type='text/x-mathjax-config'> MathJax.Hub.Config({ 'CommonHTML': { scale: 100, linebreaks: { automatic: true, width: 'container' } }, SVG: { linebreaks: { automatic:true, , width: 'container' } }, displayAlign: 'left' }); </script>")
             stb.AppendLine("<script type='text/javascript' async src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/MathJax.js?config=TeX-MML-AM_CHTML'></script>")
         End If
 
