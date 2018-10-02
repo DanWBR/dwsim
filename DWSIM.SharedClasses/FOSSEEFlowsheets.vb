a517a7de4ce8430d8b600d4efcf0e9cb8083632f
DanWBR <danielwag@gmail.com>
2018-10-02 17:16:12 -0400

    update

--- DWSIM.SharedClasses/FOSSEEFlowsheets.vb
+++ DWSIM.SharedClasses/FOSSEEFlowsheets.vb
@@ -46,6 +46,35 @@ Public Class FOSSEEFlowsheets
 
         Dim rows = htmlpage.DocumentNode.Descendants("tbody").FirstOrDefault.Descendants("tr").ToList
 
+        Dim list As New List(Of FOSSEEFlowsheet)
+        For Each r In rows
+
+            Dim fs As New FOSSEEFlowsheet
+
+            fs.Address = "http://dwsim.fossee.in" & r.ChildNodes(1).ChildNodes(0).Attributes(0).Value
+
+            response = http.GetByteArrayAsync(fs.Address)
+            response.Wait()
+
+            source = Encoding.GetEncoding("utf-8").GetString(response.Result, 0, response.Result.Length - 1)
+            source = WebUtility.HtmlDecode(source)
+
+            htmlpage.LoadHtml(source)
+
+            Dim details = htmlpage.DocumentNode.Descendants("div").Where(Function(x) x.Attributes.Contains("id") AndAlso x.Attributes("id").Value = "ajax_flowsheet_details").FirstOrDefault.ChildNodes.Descendants("li").ToList
+
+            With fs
+                .DownloadLink = htmlpage.DocumentNode.Descendants("a").Where(Function(x) x.InnerText = "Download Flowsheet").SingleOrDefault.Attributes("href").Value
+                .DWSIMVersion = r.ChildNodes(1).InnerText
+                .Institution = r.ChildNodes(1).InnerText
+                .ProposerName = r.ChildNodes(1).InnerText
+                .Reference = r.ChildNodes(1).InnerText
+                .Title = r.ChildNodes(1).InnerText
+            End With
+            list.Add(fs)
+        Next
+
+        Return list
 
     End Function
 
