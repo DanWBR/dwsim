f9c1742c099e386977d15cb05b5556b93c74c6e4
DanWBR <danielwag@gmail.com>
2020-07-30 16:43:15 -0400

    bug fix

--- DWSIM/Forms/Flowsheet/FormFlowsheet.vb
+++ DWSIM/Forms/Flowsheet/FormFlowsheet.vb
@@ -2736,11 +2736,11 @@ Public Class FormFlowsheet
         Dim cnt = TryCast(form, DockContent)
         If Not cnt Is Nothing Then
             If cnt.ShowHint = DockState.DockLeft Or cnt.ShowHint = DockState.DockLeftAutoHide Then
-                dckPanel.DockLeftPortion = 400
+                dckPanel.DockLeftPortion = 400 * Settings.DpiScale
             ElseIf cnt.ShowHint = DockState.DockRight Or cnt.ShowHint = DockState.DockRightAutoHide Then
-                dckPanel.DockRightPortion = 400
+                dckPanel.DockRightPortion = 400 * Settings.DpiScale
             ElseIf cnt.ShowHint = DockState.Float Then
-                dckPanel.DefaultFloatWindowSize = New Size(500, 500)
+                dckPanel.DefaultFloatWindowSize = New Size(500 * Settings.DpiScale, 500 * Settings.DpiScale)
             End If
             cnt.Show(Me.dckPanel)
             Try
