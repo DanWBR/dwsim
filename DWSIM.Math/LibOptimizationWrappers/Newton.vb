42807dc23e47636309a380f8620b9d645fc48b51
DanWBR <danielwag@gmail.com>
2020-12-28 11:28:17 -0400

    LibOptimization3 wrappers

---
+++ DWSIM.Math/LibOptimizationWrappers/Newton.vb
@@ -0,0 +1,113 @@
+﻿'    Copyright 2020 Daniel Wagner O. de Medeiros
+'
+'    This file is part of DWSIM.
+'
+'    DWSIM is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    DWSIM is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with DWSIM.  If not, see <http://www.gnu.org/licenses/>.
+
+Namespace MathEx.OptimizationL
+
+    Public Class Newton
+
+        Public Property Tolerance As Double = 0.0001
+
+        Public Property MaxIterations As Integer = 1000
+
+        Private _Iterations As Integer = 0
+
+        Private fxb As Func(Of Double(), Double)
+
+        Private fxg As Func(Of Double(), Double())
+
+        Private _error As Double
+
+        Private objval, objval0 As Double
+
+        Private Solutions As List(Of Double())
+
+        Private FunctionValues As List(Of Double)
+
+        Public ReadOnly Property Iterations
+            Get
+                Return _Iterations
+            End Get
+        End Property
+
+        Sub New()
+            LibOptimization.Util.clsRandomXorshiftSingleton.GetInstance.SetDefaultSeed()
+        End Sub
+
+        ''' <summary>
+        ''' Minimizes a function value using HC algorithm.
+        ''' </summary>
+        ''' <param name="functionbody">f(x) where x is a vector of doubles, returns the value of the function.</param>
+        ''' <param name="functiongradient">Optional. g(x) where x is a vector of doubles, returns the value of the gradient of the function with respect to each variable.</param>
+        ''' <param name="vars">initial values for x</param>
+        ''' <param name="lbounds">lower bounds for x</param>
+        ''' <param name="ubounds">upper bounds for x</param>
+        ''' <returns>vector of variables corresponding to the function's minimum value.</returns>
+        Public Function Solve(functionbody As Func(Of Double(), Double), functiongradient As Func(Of Double(), Double()), vars As Double(), Optional lbounds As Double() = Nothing, Optional ubounds As Double() = Nothing) As Double()
+
+            Dim obj As Double = 0.0#
+
+            Solutions = New List(Of Double())
+            FunctionValues = New List(Of Double)
+
+            fxb = functionbody
+            fxg = functiongradient
+
+            If lbounds Is Nothing Then
+                lbounds = vars.Clone()
+                For i As Integer = 0 To lbounds.Length - 1
+                    lbounds(i) = -1.0E+19
+                Next
+            End If
+
+            If ubounds Is Nothing Then
+                ubounds = vars.Clone()
+                For i As Integer = 0 To ubounds.Length - 1
+                    ubounds(i) = 1.0E+19
+                Next
+            End If
+
+            Dim optimization As New LibOptimization.Optimization.clsOptNewtonMethod(
+                New ObjectiveFunction(functionbody, functiongradient, 0.001, vars.Length))
+
+            'set initialposition
+            optimization.InitialPosition = vars
+
+            'set bpundary
+            'optimization.UpperBounds = ubounds
+            'optimization.LowerBounds = lbounds
+
+            'init
+            optimization.Init()
+            If optimization.IsRecentError() = True Then
+                Throw New Exception("Optimization error")
+            End If
+
+            'do optimization
+            While (optimization.DoIteration(MaxIterations) = False)
+                LibOptimization.Util.clsUtil.DebugValue(optimization, ai_isOutValue:=False)
+            End While
+
+            'get result
+            Return optimization.Result.ToArray()
+
+        End Function
+
+    End Class
+
+End Namespace
+
+
---
+++ DWSIM.Math/LibOptimizationWrappers/ObjectiveFunction.vb
@@ -0,0 +1,78 @@
+﻿
+Public Class ObjectiveFunction
+
+    Inherits LibOptimization.Optimization.absObjectiveFunction
+
+    Private fxb As Func(Of Double(), Double)
+
+    Private fxg As Func(Of Double(), Double())
+
+    Private epsilon As Double
+
+    Private n As Integer
+
+    Public Sub New(func1 As Func(Of Double(), Double), deriv1 As Func(Of Double(), Double()), eps As Double, nvars As Integer)
+
+        fxb = func1
+        fxg = deriv1
+        epsilon = eps
+        n = nvars
+
+    End Sub
+
+    Public Overrides Function F(ByVal a As List(Of Double)) As Double
+
+        Return fxb.Invoke(a.ToArray())
+
+    End Function
+
+    Public Overrides Function Gradient(x As List(Of Double)) As List(Of Double)
+
+        If fxg IsNot Nothing Then
+            Return fxg.Invoke(x.ToArray()).ToList()
+        Else
+            Return FunctionGradient(x.ToArray()).ToList()
+        End If
+
+    End Function
+
+    Public Overrides Function Hessian(x As List(Of Double)) As List(Of List(Of Double))
+
+        Return Nothing
+
+    End Function
+
+    Public Overrides Function NumberOfVariable() As Integer
+
+        Return n
+
+    End Function
+
+    Private Function FunctionGradient(ByVal x() As Double) As Double()
+
+        Dim f1, f2 As Double
+        Dim g(x.Length - 1), x1(x.Length - 1), x2(x.Length - 1) As Double
+        Dim j, k As Integer
+
+        For j = 0 To x.Length - 1
+            For k = 0 To x.Length - 1
+                x1(k) = x(k)
+                x2(k) = x(k)
+            Next
+            If x(j) <> 0.0# Then
+                x1(j) = x(j) * (1.0# + epsilon)
+                x2(j) = x(j) * (1.0# - epsilon)
+            Else
+                x1(j) = x(j) + epsilon
+                x2(j) = x(j) - epsilon
+            End If
+            f1 = fxb.Invoke(x1)
+            f2 = fxb.Invoke(x2)
+            g(j) = (f2 - f1) / (x2(j) - x1(j))
+        Next
+
+        Return g
+
+    End Function
+
+End Class
