0dae7b598440de8857eb2b428f2b6f446e49b1db
DanWBR <danielwag@gmail.com>
2021-01-17 14:40:24 -0400

    no need to explicitly load Reaktoro PP

--- DWSIM/Forms/Main Window/FormMain.vb
+++ DWSIM/Forms/Main Window/FormMain.vb
@@ -1673,15 +1673,6 @@ Public Class FormMain
             Try
                 xel.Element("Type").Value = xel.Element("Type").Value.Replace("DWSIM.DWSIM.SimulationObjects", "DWSIM.Thermodynamics")
                 Dim obj As PropertyPackage = Nothing
-                If xel.Element("Type").Value.Contains("AdvancedEOS") Then
-                    Dim adveoskey As String = "Reaktoro (Aqueous Electrolytes)"
-                    If PropertyPackages.ContainsKey(adveoskey) Then
-                        obj = PropertyPackages(adveoskey).ReturnInstance(xel.Element("Type").Value)
-                    Else
-                        Throw New Exception("Reaktoro (Aqueous Electrolytes) Property Package library not found. Please download and install it in order to run this simulation.")
-                    End If
-                End if
-                'ElseIf - comment ElseIf for correct loading GERG2008, PC-SAFT2, ....
                 If xel.Element("Type").Value.Contains("ThermoC") Then
                     Dim thermockey As String = "ThermoC Bridge"
                     If PropertyPackages.ContainsKey(thermockey) Then
