62d163c5d9374706a698e735ea53a26c45aace16
Jarl Robert Pedersen <jarlr.pedersen93@gmail.com>
2021-01-28 12:07:54 +0100

    Fixed Globalization issue for parsing Pipes.dat file

--- DWSIM.UnitOperations/Editing Forms/Supporting/EditingForm_Pipe_HydraulicProfileEditor.vb
+++ DWSIM.UnitOperations/Editing Forms/Supporting/EditingForm_Pipe_HydraulicProfileEditor.vb
@@ -592,8 +592,8 @@ Imports System.Drawing
 
     Public Function DN(ByVal i As Integer, ByVal k As Integer) As Object
 
-        If Double.TryParse(DNom(i, k), New Double) Then
-            Return Double.Parse(DNom(i, k), System.Globalization.CultureInfo.InvariantCulture)
+        If Double.TryParse(DNom(i, k), System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture.NumberFormat, New Double) Then
+            Return Double.Parse(DNom(i, k), System.Globalization.CultureInfo.InvariantCulture.NumberFormat)
         Else
             Return DNom(i, k)
         End If
@@ -808,6 +808,8 @@ Imports System.Drawing
         Dim idx = ToolStripMenuItem2.DropDownItems.IndexOf(e.ClickedItem)
 
         Dim r = idx + 25 - 4
+        Dim t = DN(r, 1)
+
         GridMalha.CurrentCell.Value = Format(cv.Convert("in", Units.diameter, DN(r, 1)), NumberFormat)
         GridMalha.Rows(GridMalha.CurrentRow.Index + 1).Cells(GridMalha.CurrentCell.ColumnIndex).Value = Format(cv.Convert("in", Units.diameter, DN(r, 6)), NumberFormat)
 
@@ -1058,6 +1060,7 @@ Imports System.Drawing
                 j = 0
                 Do
                     DNom(l, j) = linha_atual(j)
+                    ' JARL
                     j = j + 1
                 Loop Until j = 7
                 l = l + 1
