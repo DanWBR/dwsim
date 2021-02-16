8952eda57260e2e38e2cf86485dfd467d531bc4c
DanWBR <danielwag@gmail.com>
2021-02-16 11:38:08 -0400

    bug fix

--- DWSIM.UnitOperations/Unit Operations/RigorousColumn.vb
+++ DWSIM.UnitOperations/Unit Operations/RigorousColumn.vb
@@ -3315,8 +3315,8 @@ Namespace UnitOperations
             IObj?.SetCurrent()
 
             If TypeOf Me Is DistillationColumn Then
-                result = SolvingMethods.WangHenkeMethod.Solve(Me, nc, ns, maxits, tol, F, V, Q, L, VSS, LSS, Kval, x, y, z, fc, HF, T, P, Me.CondenserType, -1, eff, Me.ColumnType, pp, Me.Specs, False, False)
-                'result = New SolvingMethods.NaphtaliSandholmMethod().Solve(Me, nc, ns, maxits, tol, F, V, Q, L, VSS, LSS, Kval, x, y, z, fc, HF, T, P, Me.CondenserType, eff, Me.ColumnType, pp, Me.Specs, False)
+                'result = SolvingMethods.WangHenkeMethod.Solve(Me, nc, ns, maxits, tol, F, V, Q, L, VSS, LSS, Kval, x, y, z, fc, HF, T, P, Me.CondenserType, -1, eff, Me.ColumnType, pp, Me.Specs, False, False)
+                result = New SolvingMethods.NaphtaliSandholmMethod().Solve(Me, nc, ns, maxits, tol, F, V, Q, L, VSS, LSS, Kval, x, y, z, fc, HF, T, P, Me.CondenserType, eff, Me.ColumnType, pp, Me.Specs, False)
                 ic = result(9)
             ElseIf TypeOf Me Is AbsorptionColumn Then
                 If llextractor Then
@@ -6124,7 +6124,7 @@ Namespace UnitOperations.Auxiliary.SepOps.SolvingMethods
         Dim _condtype As DistillationColumn.condtype
         Dim llextr As Boolean = False
         Dim _Kval()() As Double
-        Dim _maxtchange, _Tj_ant(), _maxT, _maxvc, _maxlc As Double
+        Dim _maxtchange, _Tj_ant(), _maxT, _maxvc(), _maxlc() As Double
 
         Private grad As Boolean = False
 
@@ -6200,8 +6200,8 @@ Namespace UnitOperations.Auxiliary.SepOps.SolvingMethods
                     Throw New Exception(_pp.CurrentMaterialStream.Flowsheet.GetTranslatedString("DCGeneralError"))
                 End If
                 For j = 0 To nc - 1
-                    vc(i)(j) = x(i * (2 * nc + 1) + j + 1) * _maxvc
-                    lc(i)(j) = x(i * (2 * nc + 1) + j + 1 + nc) * _maxlc
+                    vc(i)(j) = x(i * (2 * nc + 1) + j + 1) * _maxvc(i)
+                    lc(i)(j) = x(i * (2 * nc + 1) + j + 1 + nc) * _maxlc(i)
                 Next
             Next
 
@@ -6500,6 +6500,7 @@ Namespace UnitOperations.Auxiliary.SepOps.SolvingMethods
                 Else
                     H(i) = (Hl(i) * (1 + Sl(i)) * sumlkj(i) + Hv(i) * (1 + Sv(i)) * sumvkj(i) - Hl(i - 1) * sumlkj(i - 1) - Hv(i + 1) * sumvkj(i + 1) - HF(i) * F(i) - Q(i))
                 End If
+                H(i) /= 1000.0
                 Select Case coltype
                     Case Column.ColType.DistillationColumn
                         H(0) = spfval1 / spval1
@@ -6531,10 +6532,10 @@ Namespace UnitOperations.Auxiliary.SepOps.SolvingMethods
             Dim errors(x.Length - 1) As Double
 
             For i = 0 To ns
-                errors(i * (2 * nc + 1)) = H(i) / _maxT
+                errors(i * (2 * nc + 1)) = H(i)
                 For j = 0 To nc - 1
-                    errors(i * (2 * nc + 1) + j + 1) = M(i, j) / _maxvc
-                    errors(i * (2 * nc + 1) + j + 1 + nc) = E(i, j) / _maxlc
+                    errors(i * (2 * nc + 1) + j + 1) = M(i, j)
+                    errors(i * (2 * nc + 1) + j + 1 + nc) = E(i, j)
                 Next
             Next
 
@@ -6567,6 +6568,22 @@ Namespace UnitOperations.Auxiliary.SepOps.SolvingMethods
                                 ByVal pp As PropertyPackages.PropertyPackage,
                                 ByVal specs As Dictionary(Of String, SepOps.ColumnSpec),
                               Optional ByVal LLEX As Boolean = False) As Object
+
+            Dim result As Object = Nothing
+
+            result = WangHenkeMethod.Solve(dc, nc, ns, maxits, tol, F, V, Q, L, VSS, LSS, Kval,
+                                           x, y, z, fc, HF, T, P, condt, 4, eff,
+                                           coltype, pp, specs, False, False)
+
+            T = result(0)
+            V = result(1)
+            L = result(2)
+            VSS = result(3)
+            LSS = result(4)
+            y = result(5)
+            x = result(6)
+            Kval = result(7)
+            Q = result(8)
 
             _Tj_ant = T.Clone
 
@@ -6691,27 +6708,29 @@ Namespace UnitOperations.Auxiliary.SepOps.SolvingMethods
                 Q(i) = Q(i)
             Next
 
-            Dim Sl(ns), Sv(ns) As Double
-
-            _maxvc = 0.0
-            _maxlc = 0.0
+            Dim Sl(ns), Sv(ns), maxvc(ns), maxlc(ns) As Double
 
             For i = 0 To ns
+                maxvc(i) = 0.0
+                maxlc(i) = 0.0
                 For j = 0 To nc - 1
                     vc(i)(j) = y(i)(j) * V(i)
                     lc(i)(j) = x(i)(j) * L(i)
                     xc(i)(j) = x(i)(j)
                     yc(i)(j) = y(i)(j)
                     zc(i)(j) = z(i)(j)
+                    fc(i)(j) = zc(i)(j) * F(i)
                     Tj(i) = T(i)
-                    If vc(i)(j) > _maxvc Then _maxvc = vc(i)(j)
-                    If lc(i)(j) > _maxlc Then _maxlc = lc(i)(j)
+                    If vc(i)(j) > maxvc(i) Then maxvc(i) = vc(i)(j)
+                    If lc(i)(j) > maxlc(i) Then maxlc(i) = lc(i)(j)
                 Next
                 Sv(i) = VSS(i) / V(i)
                 Sl(i) = LSS(i) / L(i)
             Next
 
             _maxT = Tj.Max
+            _maxvc = maxvc
+            _maxlc = maxlc
 
             If specs("C").SType = ColumnSpec.SpecType.Temperature Then Tj(0) = spval1
             If specs("R").SType = ColumnSpec.SpecType.Temperature Then Tj(ns) = spval2
@@ -6791,8 +6810,8 @@ Namespace UnitOperations.Auxiliary.SepOps.SolvingMethods
             For i = 0 To ns
                 xvar(i * (2 * nc + 1)) = Tj(i) / _maxT
                 For j = 0 To nc - 1
-                    xvar(i * (2 * nc + 1) + j + 1) = vc(i)(j) / _maxvc
-                    xvar(i * (2 * nc + 1) + j + 1 + nc) = lc(i)(j) / _maxlc
+                    xvar(i * (2 * nc + 1) + j + 1) = vc(i)(j) / _maxvc(i)
+                    xvar(i * (2 * nc + 1) + j + 1 + nc) = lc(i)(j) / _maxlc(i)
                 Next
             Next
 
@@ -6809,6 +6828,10 @@ Namespace UnitOperations.Auxiliary.SepOps.SolvingMethods
             Dim obj As Double = 0.0#
 
             Dim n As Integer = xvar.Length - 1
+
+            'xvar = MathNet.Numerics.RootFinding.Broyden.FindRoot(Function(xvars)
+            '                                                         Return FunctionValue(xvars)
+            '                                                     End Function, xvar)
 
             Dim nsolv As New Optimization.NewtonSolver
             nsolv.EnableDamping = True
@@ -6829,10 +6852,10 @@ Namespace UnitOperations.Auxiliary.SepOps.SolvingMethods
             End If
 
             For i = 0 To ns
-                Tj(i) = xvar(i * (2 * nc + 1))
+                Tj(i) = xvar(i * (2 * nc + 1)) * _maxT
                 For j = 0 To nc - 1
-                    vc(i)(j) = xvar(i * (2 * nc + 1) + j + 1)
-                    lc(i)(j) = xvar(i * (2 * nc + 1) + j + 1 + nc)
+                    vc(i)(j) = xvar(i * (2 * nc + 1) + j + 1) * _maxvc(i)
+                    lc(i)(j) = xvar(i * (2 * nc + 1) + j + 1 + nc) * _maxlc(i)
                 Next
             Next
 
