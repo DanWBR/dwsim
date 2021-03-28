a0c53faa9b86516d6fddd439f6d92b7f29174940
DanWBR <danielwag@gmail.com>
2021-03-28 17:48:32 -0400

    Workarounds for issue #125

--- DWSIM.UI.Desktop.Forms/Forms/Flowsheet/Flowsheet.eto.cs
+++ DWSIM.UI.Desktop.Forms/Forms/Flowsheet/Flowsheet.eto.cs
@@ -105,11 +105,13 @@ namespace DWSIM.UI.Forms
                     SpreadsheetControl?.Dispose();
                     SpreadsheetControl = null;
                 }
-                if (!ChartsControl.IsDisposed) {
+                if (!ChartsControl.IsDisposed)
+                {
                     ChartsControl?.Dispose();
                     ChartsControl = null;
                 }
-                if (!FlowsheetControl.IsDisposed) { 
+                if (!FlowsheetControl.IsDisposed)
+                {
                     FlowsheetControl?.Dispose();
                     FlowsheetControl = null;
                 }
@@ -152,7 +154,7 @@ namespace DWSIM.UI.Forms
             FlowsheetObject.Initialize();
 
             Title = "New Flowsheet";
-            
+
             if (s.FlowsheetRenderer == s.SkiaCanvasRenderer.CPU)
             {
                 FlowsheetControl = new DWSIM.UI.Controls.FlowsheetSurfaceControl() { FlowsheetObject = FlowsheetObject, FlowsheetSurface = (DWSIM.Drawing.SkiaSharp.GraphicsSurface)FlowsheetObject.GetSurface() };
@@ -1591,10 +1593,7 @@ namespace DWSIM.UI.Forms
 
                 string xmlfile = Path.ChangeExtension(Path.GetTempFileName(), "xml");
 
-                using (var fstream = new FileStream(xmlfile, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-                {
-                    FlowsheetObject.SaveToXML().Save(fstream);
-                }
+                FlowsheetObject.SaveToXML().Save(xmlfile);
 
                 var i_Files = new List<string>();
                 if (File.Exists(xmlfile))
@@ -1635,17 +1634,11 @@ namespace DWSIM.UI.Forms
             }
             else if (System.IO.Path.GetExtension(path).ToLower() == ".dwxml")
             {
-                using (var fstream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-                {
-                    FlowsheetObject.SaveToXML().Save(fstream);
-                }
+                FlowsheetObject.SaveToXML().Save(path);
             }
             else if (System.IO.Path.GetExtension(path).ToLower() == ".xml")
             {
-                using (var fstream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-                {
-                    FlowsheetObject.SaveToMXML().Save(fstream);
-                }
+                FlowsheetObject.SaveToMXML().Save(path);
             }
 
             if (!s.AutomationMode)
