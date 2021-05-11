a4f8094ebc43fc7e784f1b352758047a1eb05e4a
DanWBR <danielwag@gmail.com>
2021-05-11 11:05:34 -0400

    update

--- DWSIM.Math/LMFit.vb
+++ DWSIM.Math/LMFit.vb
@@ -7,6 +7,13 @@
             ThirdDegreePoly = 1
             FourthDegreePoly = 2
             Linear = 3
+            FifthDegreePoly = 4
+            SixthDegreePoly = 5
+            Pvap = 6
+            Cp = 7
+            LiqVisc = 8
+            HVap = 9
+            LiqDens = 10
         End Enum
 
         Private _x, _y As Double()
@@ -26,6 +33,20 @@
                     lmsolve.DefineFuncGradDelegate(AddressOf fvftp)
                 Case LMFit.FitType.Linear
                     lmsolve.DefineFuncGradDelegate(AddressOf fvlin)
+                Case LMFit.FitType.FifthDegreePoly
+                    lmsolve.DefineFuncGradDelegate(AddressOf fvfdp)
+                Case LMFit.FitType.SixthDegreePoly
+                    lmsolve.DefineFuncGradDelegate(AddressOf fvxdp)
+                Case LMFit.FitType.Pvap
+                    lmsolve.DefineFuncGradDelegate(AddressOf fvpvap)
+                Case LMFit.FitType.Cp
+                    lmsolve.DefineFuncGradDelegate(AddressOf fvcp)
+                Case LMFit.FitType.LiqVisc
+                    lmsolve.DefineFuncGradDelegate(AddressOf fvlvisc)
+                Case LMFit.FitType.HVap
+                    lmsolve.DefineFuncGradDelegate(AddressOf fvhvap)
+                Case LMFit.FitType.LiqDens
+                    lmsolve.DefineFuncGradDelegate(AddressOf fvliqdens)
             End Select
 
             Dim newc(UBound(inest) + 1) As Double
@@ -90,9 +111,9 @@
             If Double.IsNaN(fvec(1)) Or Double.IsNegativeInfinity(fvec(1)) Or Double.IsPositiveInfinity(fvec(1)) Then iflag = -1
 
             'A + B * T + C * T ^ 2
-            sum = 0.0#
             Dim i As Integer
             If iflag = 1 Then
+                sum = 0.0#
                 i = 1
                 Do
                     fvec(i) = -_y(i - 1) + (x(1) + x(2) * _x(i - 1) + x(3) * _x(i - 1) ^ 2)
@@ -120,9 +141,9 @@
             If Double.IsNaN(fvec(1)) Or Double.IsNegativeInfinity(fvec(1)) Or Double.IsPositiveInfinity(fvec(1)) Then iflag = -1
 
             'A + B * T + C * T ^ 2 + D * T ^ 3
-            sum = 0.0#
             Dim i As Integer
             If iflag = 1 Then
+                sum = 0.0#
                 i = 1
                 Do
                     fvec(i) = -_y(i - 1) + (x(1) + x(2) * _x(i - 1) + x(3) * _x(i - 1) ^ 2 + x(4) * _x(i - 1) ^ 3)
@@ -146,6 +167,167 @@
         End Sub
 
         Public Sub fvftp(ByRef x As Double(), ByRef fvec As Double(), ByRef fjac As Double(,), ByRef iflag As Integer)
+
+            If Double.IsNaN(x(1)) Or Double.IsNegativeInfinity(x(1)) Or Double.IsPositiveInfinity(x(1)) Then iflag = -1
+            If Double.IsNaN(fvec(1)) Or Double.IsNegativeInfinity(fvec(1)) Or Double.IsPositiveInfinity(fvec(1)) Then iflag = -1
+
+            'A + B * T + C * T ^ 2 + D * T ^ 3 + E * T ^ 4
+            Dim i As Integer
+            If iflag = 1 Then
+                sum = 0.0#
+                i = 1
+                Do
+                    fvec(i) = -_y(i - 1) + (x(1) + x(2) * _x(i - 1) + x(3) * _x(i - 1) ^ 2 + x(4) * _x(i - 1) ^ 3 + x(5) * _x(i - 1) ^ 4)
+                    sum += (fvec(i)) ^ 2
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            ElseIf iflag = 2 Then
+                i = 1
+                Do
+                    'A + B * T + C * T ^ 2 + D * T ^ 3 + E * T ^ 4
+                    fjac(i, 1) = 1
+                    fjac(i, 2) = _x(i - 1)
+                    fjac(i, 3) = _x(i - 1) ^ 2
+                    fjac(i, 4) = _x(i - 1) ^ 3
+                    fjac(i, 5) = _x(i - 1) ^ 4
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            End If
+
+            its += 1
+
+        End Sub
+
+        Public Sub fvfdp(ByRef x As Double(), ByRef fvec As Double(), ByRef fjac As Double(,), ByRef iflag As Integer)
+
+            If Double.IsNaN(x(1)) Or Double.IsNegativeInfinity(x(1)) Or Double.IsPositiveInfinity(x(1)) Then iflag = -1
+            If Double.IsNaN(fvec(1)) Or Double.IsNegativeInfinity(fvec(1)) Or Double.IsPositiveInfinity(fvec(1)) Then iflag = -1
+
+            'A + B * T + C * T ^ 2 + D * T ^ 3 + E * T ^ 4 + F * T ^ 5
+            Dim i As Integer
+            If iflag = 1 Then
+                sum = 0.0#
+                i = 1
+                Do
+                    fvec(i) = -_y(i - 1) + (x(1) + x(2) * _x(i - 1) + x(3) * _x(i - 1) ^ 2 + x(4) * _x(i - 1) ^ 3 + x(5) * _x(i - 1) ^ 4 + x(6) * _x(i - 1) ^ 5)
+                    sum += (fvec(i)) ^ 2
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            ElseIf iflag = 2 Then
+                i = 1
+                Do
+                    'A + B * T + C * T ^ 2 + D * T ^ 3 + E * T ^ 4 + F * T ^ 5
+                    fjac(i, 1) = 1
+                    fjac(i, 2) = _x(i - 1)
+                    fjac(i, 3) = _x(i - 1) ^ 2
+                    fjac(i, 4) = _x(i - 1) ^ 3
+                    fjac(i, 5) = _x(i - 1) ^ 4
+                    fjac(i, 6) = _x(i - 1) ^ 5
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            End If
+
+            its += 1
+
+        End Sub
+
+        Public Sub fvxdp(ByRef x As Double(), ByRef fvec As Double(), ByRef fjac As Double(,), ByRef iflag As Integer)
+
+            If Double.IsNaN(x(1)) Or Double.IsNegativeInfinity(x(1)) Or Double.IsPositiveInfinity(x(1)) Then iflag = -1
+            If Double.IsNaN(fvec(1)) Or Double.IsNegativeInfinity(fvec(1)) Or Double.IsPositiveInfinity(fvec(1)) Then iflag = -1
+
+            'A + B * T + C * T ^ 2 + D * T ^ 3 + E * T ^ 4 + F * T ^ 5 + G * T ^ 6
+            Dim i As Integer
+            If iflag = 1 Then
+                sum = 0.0#
+                i = 1
+                Do
+                    fvec(i) = -_y(i - 1) + (x(1) + x(2) * _x(i - 1) + x(3) * _x(i - 1) ^ 2 + x(4) * _x(i - 1) ^ 3 + x(5) * _x(i - 1) ^ 4 + x(6) * _x(i - 1) ^ 5 + x(7) * _x(i - 1) ^ 6)
+                    sum += (fvec(i)) ^ 2
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            ElseIf iflag = 2 Then
+                i = 1
+                Do
+                    'A + B * T + C * T ^ 2 + D * T ^ 3 + E * T ^ 4 + F * T ^ 5 + G * T ^ 6
+                    fjac(i, 1) = 1
+                    fjac(i, 2) = _x(i - 1)
+                    fjac(i, 3) = _x(i - 1) ^ 2
+                    fjac(i, 4) = _x(i - 1) ^ 3
+                    fjac(i, 5) = _x(i - 1) ^ 4
+                    fjac(i, 6) = _x(i - 1) ^ 5
+                    fjac(i, 7) = _x(i - 1) ^ 6
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            End If
+
+            its += 1
+
+        End Sub
+
+        Public Sub fvlin(ByRef x As Double(), ByRef fvec As Double(), ByRef fjac As Double(,), ByRef iflag As Integer)
+
+            If Double.IsNaN(x(1)) Or Double.IsNegativeInfinity(x(1)) Or Double.IsPositiveInfinity(x(1)) Then iflag = -1
+            If Double.IsNaN(fvec(1)) Or Double.IsNegativeInfinity(fvec(1)) Or Double.IsPositiveInfinity(fvec(1)) Then iflag = -1
+
+            'A + B * T
+            Dim i As Integer
+            If iflag = 1 Then
+                sum = 0.0#
+                i = 1
+                Do
+                    fvec(i) = -_y(i - 1) + (x(1) + x(2) * _x(i - 1))
+                    sum += (fvec(i)) ^ 2
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            ElseIf iflag = 2 Then
+                i = 1
+                Do
+                    'A + B * T
+                    fjac(i, 1) = 1
+                    fjac(i, 2) = _x(i - 1)
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            End If
+
+            its += 1
+
+        End Sub
+
+        Public Sub fvpvap(ByRef x As Double(), ByRef fvec As Double(), ByRef fjac As Double(,), ByRef iflag As Integer)
+
+            If Double.IsNaN(x(1)) Or Double.IsNegativeInfinity(x(1)) Or Double.IsPositiveInfinity(x(1)) Then iflag = -1
+            If Double.IsNaN(fvec(1)) Or Double.IsNegativeInfinity(fvec(1)) Or Double.IsPositiveInfinity(fvec(1)) Then iflag = -1
+
+            sum = 0.0#
+            Dim i As Integer
+            If iflag = 1 Then
+                i = 1
+                Do
+                    fvec(i) = -_y(i - 1) + (Math.Exp(x(1) + x(2) / _x(i - 1) + x(3) * Math.Log(_x(i - 1)) + x(4) * _x(i - 1) ^ x(5)))
+                    sum += (fvec(i)) ^ 2
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            ElseIf iflag = 2 Then
+                Dim fval As Double = 0
+                i = 1
+                Do
+                    'Math.Exp(A + B / T + C * Math.Log(T) + D * T ^ E)
+                    fval = (Math.Exp(x(1) + x(2) / _x(i - 1) + x(3) * Math.Log(_x(i - 1)) + x(4) * _x(i - 1) ^ x(5)))
+                    fjac(i, 1) = fval
+                    fjac(i, 2) = fval * 1 / _x(i - 1)
+                    fjac(i, 3) = fval * Math.Log(_x(i - 1))
+                    fjac(i, 4) = fval * _x(i - 1) ^ x(5)
+                    fjac(i, 5) = fval * x(5) * _x(i - 1) ^ x(5) * Math.Log(_x(i - 1))
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            End If
+
+            its += 1
+
+        End Sub
+
+        Public Sub fvcp(ByRef x As Double(), ByRef fvec As Double(), ByRef fjac As Double(,), ByRef iflag As Integer)
 
             If Double.IsNaN(x(1)) Or Double.IsNegativeInfinity(x(1)) Or Double.IsPositiveInfinity(x(1)) Then iflag = -1
             If Double.IsNaN(fvec(1)) Or Double.IsNegativeInfinity(fvec(1)) Or Double.IsPositiveInfinity(fvec(1)) Then iflag = -1
@@ -177,27 +359,96 @@
 
         End Sub
 
-        Public Sub fvlin(ByRef x As Double(), ByRef fvec As Double(), ByRef fjac As Double(,), ByRef iflag As Integer)
+        Public Sub fvlvisc(ByRef x As Double(), ByRef fvec As Double(), ByRef fjac As Double(,), ByRef iflag As Integer)
 
             If Double.IsNaN(x(1)) Or Double.IsNegativeInfinity(x(1)) Or Double.IsPositiveInfinity(x(1)) Then iflag = -1
             If Double.IsNaN(fvec(1)) Or Double.IsNegativeInfinity(fvec(1)) Or Double.IsPositiveInfinity(fvec(1)) Then iflag = -1
 
-            'A + B * T
+            sum = 0
+            Dim i As Integer
+            If iflag = 1 Then
+                i = 1
+                Do
+                    fvec(i) = -_y(i - 1) + (Math.Exp(x(1) + x(2) / _x(i - 1) + x(3) * Math.Log(_x(i - 1)) + x(4) * _x(i - 1) ^ x(5)))
+                    sum += (fvec(i)) ^ 2
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            ElseIf iflag = 2 Then
+                Dim fval As Double = 0
+                i = 1
+                Do
+                    'Math.Exp(A + B / T + C * Math.Log(T) + D * T ^ E)
+                    fval = (Math.Exp(x(1) + x(2) / _x(i - 1) + x(3) * Math.Log(_x(i - 1)) + x(4) * _x(i - 1) ^ x(5)))
+                    fjac(i, 1) = fval
+                    fjac(i, 2) = fval * 1 / _x(i - 1)
+                    fjac(i, 3) = fval * Math.Log(_x(i - 1))
+                    fjac(i, 4) = fval * _x(i - 1) ^ x(5)
+                    fjac(i, 5) = fval * x(5) * _x(i - 1) ^ x(5) * Math.Log(_x(i - 1))
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            End If
+
+            its += 1
+
+        End Sub
+
+        Public Sub fvhvap(ByRef x As Double(), ByRef fvec As Double(), ByRef fjac As Double(,), ByRef iflag As Integer)
+
+            If Double.IsNaN(x(1)) Or Double.IsNegativeInfinity(x(1)) Or Double.IsPositiveInfinity(x(1)) Then iflag = -1
+            If Double.IsNaN(fvec(1)) Or Double.IsNegativeInfinity(fvec(1)) Or Double.IsPositiveInfinity(fvec(1)) Then iflag = -1
+
+            'A * (1 - Tr) ^ (B + C * Tr + D * Tr ^ 2)
             sum = 0.0#
             Dim i As Integer
             If iflag = 1 Then
                 i = 1
                 Do
-                    fvec(i) = -_y(i - 1) + (x(1) + x(2) * _x(i - 1))
+                    fvec(i) = -_y(i - 1) + (x(1) * (1 - _x(i - 1)) ^ (x(2) + x(3) * _x(i - 1) + x(4) * _x(i - 1) ^ 2))
                     sum += (fvec(i)) ^ 2
                     i = i + 1
                 Loop Until i = UBound(_y) + 2
             ElseIf iflag = 2 Then
                 i = 1
                 Do
-                    'A + B * T
-                    fjac(i, 1) = 1
-                    fjac(i, 2) = _x(i - 1)
+                    Dim fval As Double = 0
+                    'A * (1 - Tr) ^ (B + C * Tr + D * Tr ^ 2)
+                    fval = (x(1) * (1 - _x(i - 1)) ^ (x(2) + x(3) * _x(i - 1) + x(4) * _x(i - 1) ^ 2))
+                    fjac(i, 1) = fval
+                    fjac(i, 2) = fval
+                    fjac(i, 3) = fval * _x(i - 1)
+                    fjac(i, 4) = fval * _x(i - 1) ^ 2
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            End If
+
+            its += 1
+
+        End Sub
+
+        Public Sub fvliqdens(ByRef x As Double(), ByRef fvec As Double(), ByRef fjac As Double(,), ByRef iflag As Integer)
+
+            If Double.IsNaN(x(1)) Or Double.IsNegativeInfinity(x(1)) Or Double.IsPositiveInfinity(x(1)) Then iflag = -1
+            If Double.IsNaN(fvec(1)) Or Double.IsNegativeInfinity(fvec(1)) Or Double.IsPositiveInfinity(fvec(1)) Then iflag = -1
+
+            'a / b^[1 + (1 - t/c)^d]
+            sum = 0.0#
+            Dim i As Integer
+            If iflag = 1 Then
+                i = 1
+                Do
+                    fvec(i) = -_y(i - 1) + (x(1) / x(2) ^ (1 + (1 - _x(i - 1) / x(3)) ^ x(4)))
+                    sum += (fvec(i)) ^ 2
+                    i = i + 1
+                Loop Until i = UBound(_y) + 2
+            ElseIf iflag = 2 Then
+                i = 1
+                Do
+                    'a / b^[1 + (1 - t/c)^d]
+                    fjac(i, 1) = 1 / x(2) ^ (1 + (1 - _x(i - 1) / x(3)) ^ x(4))
+                    fjac(i, 2) = -(x(1) * (x(3) - _x(i - 1)) ^ x(4) + x(1) * x(3) ^ x(4)) / (x(2) ^ (((x(3) - _x(i - 1)) ^ x(4) + 2 * x(3) ^ x(4)) / x(3) ^ x(4)) * x(3) ^ x(4))
+                    fjac(i, 3) = x(1) * Math.Log(x(2)) * x(4) * (x(3) - _x(i - 1)) ^ x(4) * _x(i - 1) / (x(2) ^ (((x(3) - _x(i - 1)) ^ x(4) + x(3) ^ x(4)) / x(3) ^ x(4)) * x(3) ^ (x(4) + 1) * _x(i - 1) - x(2) ^ (((x(3) - _x(i - 1)) ^ x(4) + x(3) ^ x(4)) / x(3) ^ x(4)) * x(3) ^ (x(4) + 2))
+                    fjac(i, 4) = -(x(1) * Math.Log(x(2)) * Math.Log(x(3) - _x(i - 1)) - x(1) * Math.Log(x(2)) * Math.Log(x(3))) * (x(3) - _x(i - 1)) ^ x(4) / (x(2) ^ (((x(3) - _x(i - 1)) ^ x(4) + x(3) ^ x(4)) / x(3) ^ x(4)) * x(3) ^ x(4))
+                    fjac(i, 5) = 0
                     i = i + 1
                 Loop Until i = UBound(_y) + 2
             End If
