1f738ff0d0096f718c8a5bb46474c4a89f32bc75
DanWBR <danielwag@gmail.com>
2021-06-20 11:40:31 -0400

    bugfix max dropdown width

--- DWSIM.ExtensionMethods/DropDownWidth.vb
+++ DWSIM.ExtensionMethods/DropDownWidth.vb
@@ -7,15 +7,15 @@ Public Module DropDownWidth
 
         Dim maxWidth As Integer = 0, temp As Integer = 0
 
-        For Each obj In myCombo.Items
-            temp = TextRenderer.MeasureText(obj.ToString(), myCombo.Font).Width
+        Try
+            For Each obj In myCombo.Items
+                temp = TextRenderer.MeasureText(obj.ToString(), myCombo.Font).Width
+                If temp > maxWidth Then maxWidth = temp
+            Next
+        Catch ex As Exception
+        End Try
 
-            If temp > maxWidth Then
-                maxWidth = temp
-            End If
-        Next
-
-        myCombo.DropDownWidth = maxWidth
+        If maxWidth > 0 Then myCombo.DropDownWidth = maxWidth
 
     End Sub
 
