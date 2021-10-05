dc4510afc9291cf78c45132b63986b9ee19cd73d
DanWBR <danielwag@gmail.com>
2021-10-05 08:04:07 -0300

    error logging library

---
+++ DWSIM.Logging/Logger.cs
@@ -0,0 +1,63 @@
+﻿using DWSIM.GlobalSettings;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWSIM.Logging
+{
+    public class Logger
+    {
+
+        private static NLog.Logger logger;
+
+        public static void Initialize()
+        {
+
+            var logfiledir = "";
+            if (GlobalSettings.Settings.RunningPlatform() == Settings.Platform.Mac)
+            {
+                logfiledir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Documents", "DWSIM Application Data");
+            }
+            else
+            {
+                logfiledir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "DWSIM Application Data");
+            }
+            if (!Directory.Exists(logfiledir)) Directory.CreateDirectory(logfiledir);
+
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = Path.Combine(logfiledir, "errors.log") };
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logfile);
+
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+
+            logger = NLog.LogManager.GetCurrentClassLogger();
+
+        }
+
+        public static void LogError(string message, Exception ex)
+        {
+            logger.Error(ex, message);
+        }
+
+        public static void LogUnhandled(string message, Exception ex)
+        {
+            logger.Fatal(ex, message);
+        }
+
+        public static void LogWarning(string message, Exception ex)
+        {
+            logger.Warn(ex, message);
+        }
+
+
+    }
+}
---
+++ DWSIM.Logging/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DWSIM Error Logging Library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DWSIM Project")]
+[assembly: AssemblyProduct("DWSIM")]
+[assembly: AssemblyCopyright("Copyright © 2021 Daniel Medeiros")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b176a143-2822-4e7c-9280-cf4a615e1c35")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("6.6.3.0")]
+[assembly: AssemblyFileVersion("6.6.3.0")]
