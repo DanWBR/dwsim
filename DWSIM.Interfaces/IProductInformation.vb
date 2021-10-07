d3975080e1d53f66ce48a26def49e0bd852b1b51
DanWBR <danielwag@gmail.com>
2021-10-07 11:01:49 -0300

    IProductInformation

---
+++ DWSIM.Interfaces/IProductInformation.vb
@@ -0,0 +1,15 @@
+﻿Public Interface IProductInformation
+
+    ReadOnly Property ProductName As String
+
+    ReadOnly Property ProductDescription As String
+
+    ReadOnly Property ProductAuthor As String
+
+    ReadOnly Property ProductContactInfo As String
+
+    ReadOnly Property ProductPage As String
+
+    ReadOnly Property ProductVersion As String
+
+End Interface
