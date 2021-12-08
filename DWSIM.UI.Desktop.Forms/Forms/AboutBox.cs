2869fc26ae9fb273c30e36336be3eba636abede9
andr1976 <58475535+andr1976@users.noreply.github.com>
2021-12-08 17:28:41 +0100

    Adding GERG 2008 license to About box

--- DWSIM.UI.Desktop.Forms/Forms/AboutBox.cs
+++ DWSIM.UI.Desktop.Forms/Forms/AboutBox.cs
@@ -241,6 +241,7 @@ namespace DWSIM.UI.Forms.Forms
         private void AddComponentInfo()
         {
             components.Add(new ComponentInfo("CoolProp", "6.0.0", "2016", "Ian H. Bell", "http://wwww.coolprop.org", "MIT-style License", "https://github.com/ibell/coolprop/blob/master/LICENSE"));
+            components.Add(new ComponentInfo("GERG 2008", "2.0", "2017", "E. W. Lemmon, V. Heinemann, J. Lu, I. Bell", "https://github.com/usnistgov/AGA8", "NIST/17 U.S.C. 105", "https://github.com/usnistgov/AGA8/blob/master/LICENSE"));
             components.Add(new ComponentInfo("ChemSep Database", "7.30", "2018", "Harry Kooijman, Ross Taylor", "http://www.chemsep.org", "Perl Artistic License v2", "http://www.perlfoundation.org/artistic_license_2_0"));
             components.Add(new ComponentInfo("Flee", "0.9.14", "2009", "Eugene Ciloci", "https://flee.codeplex.com", "LGPLv2", "http://www.gnu.org/licenses/lgpl.html"));
             components.Add(new ComponentInfo("DotNumerics", "1.0", "2009", "Jose Antonio De Santiago Castillo", "http://www.dotnumerics.com", "GPLv3", "http://www.gnu.org/licenses/gpl.html"));
