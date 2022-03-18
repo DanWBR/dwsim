678414e257eba2fc62bf3ee2ffe9d57ec43606db
DanWBR <danielwag@gmail.com>
2022-03-18 11:57:08 -0400

    flowsheet and spreadsheet uo - use embedded files

--- DWSIM/Forms/FormLoadingSimulation.vb
+++ DWSIM/Forms/FormLoadingSimulation.vb
@@ -2,10 +2,6 @@
 
     Public Sub FormLoadingSimulation_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-
-        Label1.Parent = ProgressBar1
-        Label1.BackColor = Color.Transparent
-
         ExtensionMethods.ChangeDefaultFont(Me)
 
     End Sub
