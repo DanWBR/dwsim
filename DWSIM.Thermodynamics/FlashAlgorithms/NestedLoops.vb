7e76b5330b24e4401a137ee8645b19d008eb7df6
DanWBR <danielwag@gmail.com>
2022-06-23 18:33:37 -0400

    tentative fix for #333

--- DWSIM.Thermodynamics/FlashAlgorithms/NestedLoops.vb
+++ DWSIM.Thermodynamics/FlashAlgorithms/NestedLoops.vb
@@ -2151,6 +2151,13 @@ out:        WriteDebugInfo("PT Flash [NL]: Converged in " & ecount & " iteration
             Dim result As Object()
 
             result = Flash_PV_1(Vz, P, V, Tref, PP, ReuseKI, PrevKi)
+            'check if converged to the trivial solution.
+            Dim Kvals As Double() = result(6)
+            Dim Tt As Double = result(4)
+            If PP.AUX_CheckTrivial(Kvals, 0.75) Then
+                'try again with a slightly different temperature to get out of trivial region
+                result = Flash_PV_1(Vz, P, V, Tt * 1.2, PP, ReuseKI, PrevKi)
+            End If
             If result.Count = 1 Then
                 result = Flash_PV_1(Vz, P, V, 0.0, PP, False, Nothing)
             End If
