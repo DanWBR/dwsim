9e3fc46bc4f134552eeea5f17c9bdcb881d8c7ad
Mersad Katana <mersadk@gmail.com>
2023-03-14 13:05:23 +0100

    Some changes

--- DWSIM.UI.Web/Settings/DashboardSettings.cs
+++ DWSIM.UI.Web/Settings/DashboardSettings.cs
@@ -13,6 +13,7 @@ namespace DWSIM.UI.Web.Settings
         public static string ExcelRunnerServiceUrl = "https://excel-runner.azurewebsites.net";
         public static string SensitivityStudiesServiceUrl = "https://sensitivity-studies.azurewebsites.net";
         public static string TakeHomeExamsServiceUrl = "https://take-home-exams.azurewebsites.net";
+        public static string Environment = "Production";
 
         static DashboardSettings()
         {
@@ -23,6 +24,7 @@ namespace DWSIM.UI.Web.Settings
                 ExcelRunnerServiceUrl = "https://excel-runner-staging.azurewebsites.net";
                 SensitivityStudiesServiceUrl = "https://sensitivity-studies-staging.azurewebsites.net";
                 TakeHomeExamsServiceUrl = "https://take-home-exams-staging.azurewebsites.net";
+                Environment = "Staging";
             }
         }
     }
