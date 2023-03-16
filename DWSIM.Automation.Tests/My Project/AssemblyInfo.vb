6c5b133c060d1ed3a3a03f4538dcb0f27e6e4e3b
DanWBR <danielwag@gmail.com>
2023-03-16 10:05:13 -0400

    v8.4.2 RC1

--- DWSIM.Automation.Tests/My Project/AssemblyInfo.vb
+++ DWSIM.Automation.Tests/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("8.4.1.0")>
-<Assembly: AssemblyFileVersion("8.4.1.0")>
+<Assembly: AssemblyVersion("8.4.2.0")>
+<Assembly: AssemblyFileVersion("8.4.2.0")>
