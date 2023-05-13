100bfa29cfd5a76fb11e5c92646766266a05156f
Daniel Wagner Oliveira de Medeiros <danielwag@gmail.com>
2023-05-13 11:13:44 -0400

    ui

--- DWSIM/Forms/FormBrowser.vb
+++ DWSIM/Forms/FormBrowser.vb
@@ -1,4 +1,7 @@
-﻿Public Class FormBrowser
+﻿Imports Eto.Forms
+Imports Microsoft.Web.WebView2.Core
+
+Public Class FormBrowser
 
     Private Sub FormBrowser_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -8,12 +11,17 @@
 
         If title <> "" Then Text = title
 
-        Viewer.EnsureCoreWebView2Async(FormMain.WebView2Environment).ContinueWith(Sub()
-                                                                                      UIThread(Sub()
-                                                                                                   Viewer.Source = New Uri(url)
-                                                                                                   Me.Activate()
-                                                                                               End Sub)
-                                                                                  End Sub)
+
+        Viewer.EnsureCoreWebView2Async(FormMain.WebView2Environment).ContinueWith(
+            Sub()
+                UIThread(Sub()
+                             Viewer.CoreWebView2.Settings.HiddenPdfToolbarItems = CoreWebView2PdfToolbarItems.Save +
+                                                                                  CoreWebView2PdfToolbarItems.SaveAs +
+                                                                                  CoreWebView2PdfToolbarItems.Print
+                             Viewer.Source = New Uri(url)
+                             Me.Activate()
+                         End Sub)
+            End Sub)
 
     End Sub
 
@@ -21,12 +29,18 @@
 
         If title <> "" Then Text = title
 
-        Viewer.EnsureCoreWebView2Async(FormMain.WebView2Environment).ContinueWith(Sub()
-                                                                                      UIThread(Sub()
-                                                                                                   Viewer.NavigateToString(html)
-                                                                                                   Me.Activate()
-                                                                                               End Sub)
-                                                                                  End Sub)
+        Viewer.CoreWebView2.Settings.HiddenPdfToolbarItems = CoreWebView2PdfToolbarItems.Save And CoreWebView2PdfToolbarItems.SaveAs
+
+        Viewer.EnsureCoreWebView2Async(FormMain.WebView2Environment).ContinueWith(
+            Sub()
+                UIThread(Sub()
+                             Viewer.CoreWebView2.Settings.HiddenPdfToolbarItems = CoreWebView2PdfToolbarItems.Save +
+                                                                                  CoreWebView2PdfToolbarItems.SaveAs +
+                                                                                  CoreWebView2PdfToolbarItems.Print
+                             Viewer.NavigateToString(html)
+                             Me.Activate()
+                         End Sub)
+            End Sub)
 
     End Sub
 
