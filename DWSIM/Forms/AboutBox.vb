0a22e0714806801f04bd712db1d31d86cb3b2b66
DanWBR <danielwag@gmail.com>
2023-05-24 14:24:24 -0400

    update

--- DWSIM/Forms/AboutBox.vb
+++ DWSIM/Forms/AboutBox.vb
@@ -22,6 +22,8 @@ Public Class AboutBox
         TextBox1.Font = New Font("Consolas", 9, GraphicsUnit.Point)
         TextBox2.Font = New Font("Consolas", 9, GraphicsUnit.Point)
 
+        TextBox2.Text = "Special Thanks to the following Patrons: " + Patrons.GetList() + vbCrLf + vbCrLf + TextBox2.Text
+
         Dim updfile = My.Application.Info.DirectoryPath & Path.DirectorySeparatorChar & "version.info"
 
         Version.Text = "Version " & My.Application.Info.Version.Major & "." & My.Application.Info.Version.Minor & "." & My.Application.Info.Version.Build
