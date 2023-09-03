76d68d2670967a0c375c6f1cd80cb133022a118f
Daniel Medeiros <danielwag@gmail.com>
2023-09-03 08:50:54 -0400

    expose indicators' monitored values

--- DWSIM.UnitOperations/Indicators/AnalogGauge.vb
+++ DWSIM.UnitOperations/Indicators/AnalogGauge.vb
@@ -132,13 +132,25 @@ Namespace UnitOperations
 
         Public Overrides Function GetPropertyValue(ByVal prop As String, Optional ByVal su As Interfaces.IUnitsOfMeasure = Nothing) As Object
 
-            Return ""
+            Try
+
+                Dim SelectedObject = GetFlowsheet.SimulationObjects.Values.Where(Function(x) x.Name = SelectedObjectID).FirstOrDefault
+
+                Dim currentvalue = SystemsOfUnits.Converter.ConvertFromSI(SelectedPropertyUnits, SelectedObject.GetPropertyValue(SelectedProperty))
+
+                Return currentvalue
+
+            Catch ex As Exception
+
+                Return Double.NaN
+
+            End Try
 
         End Function
 
         Public Overloads Overrides Function GetProperties(ByVal proptype As Interfaces.Enums.PropertyType) As String()
 
-            Return New String() {}
+            Return New String() {"Monitored Value"}
 
         End Function
 
@@ -150,7 +162,7 @@ Namespace UnitOperations
 
         Public Overrides Function GetPropertyUnit(ByVal prop As String, Optional ByVal su As Interfaces.IUnitsOfMeasure = Nothing) As String
 
-            Return ""
+            Return SelectedPropertyUnits
 
         End Function
 
