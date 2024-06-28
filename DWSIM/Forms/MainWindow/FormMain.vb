6b2916e0ef1af41c43fbe781b3b2a4b30b5e2293
Senad Mulaosmanovic <sm@capital-gain.eu>
2024-06-28 10:15:54 +0200

    When saving to Simulate 365 offer to overwrite file if its existing

--- DWSIM/Forms/MainWindow/FormMain.vb
+++ DWSIM/Forms/MainWindow/FormMain.vb
@@ -4214,6 +4214,7 @@ Label_00CC:
             Dim form2 As FormFlowsheet = Me.ActiveMdiChild
 
             Dim filename = form2.Options.FilePath
+            Dim shouldOverwriteFile As Boolean = False
 
             Dim filePickerForm As IFilePicker
 
@@ -4223,6 +4224,13 @@ Label_00CC:
                     Dim fname = Path.GetFileNameWithoutExtension(form2.Options.FilePath)
                     filePickerForm.SuggestedFilename = fname
                     If form2.Options.VirtualFile IsNot Nothing Then
+                        Dim shouldOverwriteExistingFileResult As DialogResult = MessageBox.Show("Do you want to overwrite the existing file?", "Save file", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+                        If (shouldOverwriteExistingFileResult = DialogResult.Yes) Then
+                            shouldOverwriteFile = True
+                        End If
+
+
                         filePickerForm.SuggestedDirectory = form2.Options.VirtualFile.ParentUniqueIdentifier
                     End If
 
@@ -4242,12 +4250,17 @@ Label_00CC:
                 End Try
             End If
 
-            Dim handler As IVirtualFile = filePickerForm.ShowSaveDialog(
-            New List(Of SharedClassesCSharp.FilePicker.FilePickerAllowedType) From
-            {New SharedClassesCSharp.FilePicker.FilePickerAllowedType("Compressed XML Simulation File", "*.dwxmz"),
-            New SharedClassesCSharp.FilePicker.FilePickerAllowedType("XML Simulation File", "*.dwxml"),
-            New SharedClassesCSharp.FilePicker.FilePickerAllowedType("Interchangeable PFD Simulation File", "*.pfdx"),
-            New SharedClassesCSharp.FilePicker.FilePickerAllowedType("Mobile XML Simulation File", "*.xml")})
+            Dim handler As IVirtualFile = Nothing
+            If shouldOverwriteFile Then
+                handler = form2.Options.VirtualFile
+            Else
+                handler = filePickerForm.ShowSaveDialog(
+                New List(Of SharedClassesCSharp.FilePicker.FilePickerAllowedType) From
+                {New SharedClassesCSharp.FilePicker.FilePickerAllowedType("Compressed XML Simulation File", "*.dwxmz"),
+                New SharedClassesCSharp.FilePicker.FilePickerAllowedType("XML Simulation File", "*.dwxml"),
+                New SharedClassesCSharp.FilePicker.FilePickerAllowedType("Interchangeable PFD Simulation File", "*.pfdx"),
+                New SharedClassesCSharp.FilePicker.FilePickerAllowedType("Mobile XML Simulation File", "*.xml")})
+            End If
 
             If handler IsNot Nothing Then
 
